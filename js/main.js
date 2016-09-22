7acc86128158f476ed856dc354768bbe6c388c49
Karen Volkman <karenv@cosmetics.local>
2016-09-22 15:40:58 -0700

    additional styling

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+      function initMap() {
+        var myLatLng = {lat: 40.8054491, lng: -73.9654415};
 
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+          zoom: 17
+        });
+
+        // Create a marker and set its position.
+        var marker = new google.maps.Marker({
+          map: map,
+          position: myLatLng,
+          title: 'Our Restaurant'
+        });
+      }
 
